3168c1c9b5ff4e94e1934231b4c2c3444c653258
unknown <fisers@LCANB722.dds.dillards.net>
2013-06-26 09:47:18 -0500

    added namespace

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-$(document).ready(function() {
-	
-	var passtext = "password";
-	document.getElementById("passwordInput").type = 'text';
+var PlacholderJS = {
 
 	// For the following code to work as intended, ensure that the data-content and value attr 
-	//of the html input you are working with match
+	// of the html input you are working with match
 
-	function inputFocus(element) {
+	inputsInit: function() {
+			document.getElementById("passwordInput").type = 'text';
+			var username = $('#userIdInput');
+			var password = $('#passwordInput');
+			/* Attach Listeners	*/
+			PlacholderJS.inputFocus(username);
+			PlacholderJS.inputFocus(password);
+			PlacholderJS.inputBlur(username);
+			PlacholderJS.inputBlur(password);
+	},
+	inputFocus: function(element) {
 		$(element).focus(function() {
 			if (this.value == $(this).data("content")) {
 					this.value = '';
-				if ($(this).data("content").indexOf(passtext) != -1) {
+				if ($(this).data("content").indexOf("password") != -1) {
 					this.type = 'password';
 				}
 			}
 		});
-	}
-
-	function inputBlur(element) {
+	},
+	inputBlur: function(element) {
 		$(element).blur(function() {
 		if (this.value == '') {
 				this.value = $(this).data("content");
-				if ($(this).data("content").indexOf(passtext) != -1) {
+				if ($(this).data("content").indexOf("password") != -1) {
 					this.type = 'text';
 				}
 			}
 		});
 	}
 
+};
 
-
-	var username = $('#userIdInput');
-	var password = $('#passwordInput');
-
-	/* Attach Listeners	*/
-	inputFocus(username);
-	inputFocus(password);
-	inputBlur(username);
-	inputBlur(password);
-
-});
+window.onload = PlacholderJS.inputsInit;
+	
