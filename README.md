601b8954678f286c103e0bbe30afe2cfc2ff180f
unknown <fisers@LCANB722.dds.dillards.net>
2013-07-02 14:02:59 -0500

    fixed for ie8

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-The original intent of this little plugin was to be able to use placeholder text in older browsers that do not support html input placeholders.
+This is a small javascript module that allows you to add placeholders to older browsers that don't support the "placeholder" attribute for user input.
 
-However, IE8 and back do not support changing the type of text box dynamically. I'm working up a new solution.
-
-Check out the current version at http://jsfiddle.net/StephenFiser/9V9jX/
+Check it out at http://jsfiddle.net/StephenFiser/9V9jX/
--- script.js
+++ script.js
@@ -10,22 +10,26 @@ var PlacholderJS = {
 				return;
 			}
 
-			document.getElementById("passwordInput").type = 'text';
+			
 
-			var username = $('#userIdInput'), password = $('#passwordInput'), passText = "password";
+
+			var username = $('#userIdInput'), passIn = $('#passwordInput'), passText = "password", passHold = $('#passwordHolder');
 			username.blur(); // need this since different browsers focus on first element differently
+			passIn.hide();
 			// Attach Listeners	
 			PlacholderJS.inputFocus(username, passText);
-			PlacholderJS.inputFocus(password, passText);
 			PlacholderJS.inputBlur(username, passText);
-			PlacholderJS.inputBlur(password, passText);
+			PlacholderJS.inputFocus(passHold, passText);
+			PlacholderJS.inputBlur(passIn, passText);
 	},
 	inputFocus: function(element, testText) {
 		$(element).focus(function() {
 			if (this.value == $(this).data("content")) {
 					this.value = '';
 				if ($(this).data("content").indexOf(testText) != -1) {
-					this.type = 'password';
+					$(this).hide();
+					$('#passwordInput')[0].value = '';
+					$('#passwordInput').show().focus();
 				}
 			}
 		});
@@ -35,7 +39,9 @@ var PlacholderJS = {
 		if (this.value == '') {
 				this.value = $(this).data("content");
 				if ($(this).data("content").indexOf(testText) != -1) {
-					this.type = 'text';
+					$(this).hide();
+					$('#passwordHolder').show();
+					$('#passwordHolder')[0].value = $('#passwordHolder').data("content");
 				}
 			}
 		});
